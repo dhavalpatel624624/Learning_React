a49256f3d6cb9e021817367c1f845dc1a6f5560d
Patel <Dhaval.Patel@vwcredit.com>
2019-02-26 10:31:39 -0600

    setup webpack, babel, and react. Converted Classes.JS to arrow functions

--- Learning-Basics/Classes.js
+++ Learning-Basics/Classes.js
@@ -1,28 +1,23 @@
 class Human{
-    constructor(){
-        this.gender = "Male";
-    }
+    gender = "Male";
 
-    printGender(){
+    printGender = () => {
         console.log(this.gender);
     }
 }
 
 class Person extends Human{
-    constructor(){
-        super();
-        this.name = "Mike";
+    
+    name = "Mike";
+    //change property from male to female
+    gender = "Female";
 
-        //change property from male to female
-        this.gender = "Female";
-    }
-
-    printMyName(){
-        console.log(this.name);
-    }
+    printMyName= () => {console.log(this.name);}
 }
 
 const person = new Person();
 
 person.printGender();
 person.printMyName();
+
+export default Person;
